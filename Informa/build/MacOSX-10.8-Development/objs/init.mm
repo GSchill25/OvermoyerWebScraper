d22c489c195868ca9144d1c8e5bf88ec2a4d498d
Matthew Nielsen <Matthew@matthews-mbp.asusrouter>
2013-11-14 22:38:07 -0500

    build directory added

---
+++ Informa/build/MacOSX-10.8-Development/objs/init.mm
@@ -0,0 +1,46 @@
+extern "C" {
+    void ruby_sysinit(int *, char ***);
+    void ruby_init(void);
+    void ruby_init_loadpath(void);
+    void ruby_script(const char *);
+    void ruby_set_argv(int, char **);
+    void rb_vm_init_compiler(void);
+    void rb_vm_init_jit(void);
+    void rb_vm_aot_feature_provide(const char *, void *);
+    void *rb_vm_top_self(void);
+    void rb_define_global_const(const char *, void *);
+    void rb_rb2oc_exc_handler(void);
+    void rb_exit(int);
+void MREP_2AAB5F4224D64D91A8DECF20E9B3BA12(void *, void *);
+void MREP_C11C809B3FFC4BAA9AFC297F63CE3653(void *, void *);
+}
+
+extern "C"
+void
+RubyMotionInit(int argc, char **argv)
+{
+    static bool initialized = false;
+    if (!initialized) {
+	ruby_init();
+	ruby_init_loadpath();
+        if (argc > 0) {
+	    const char *progname = argv[0];
+	    ruby_script(progname);
+	}
+#if !__LP64__
+	try {
+#endif
+	    void *self = rb_vm_top_self();
+rb_define_global_const("RUBYMOTION_ENV", @"development");
+rb_define_global_const("RUBYMOTION_VERSION", @"2.14");
+MREP_2AAB5F4224D64D91A8DECF20E9B3BA12(self, 0);
+MREP_C11C809B3FFC4BAA9AFC297F63CE3653(self, 0);
+#if !__LP64__
+	}
+	catch (...) {
+	    rb_rb2oc_exc_handler();
+	}
+#endif
+	initialized = true;
+    }
+}
